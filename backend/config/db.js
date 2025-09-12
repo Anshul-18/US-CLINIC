d56174d498bd212f70c1d22455fcb242a838e30b
Archit Shrivas <133688162+Archit026@users.noreply.github.com>
2025-09-12 14:49:19 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/dentistApp');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/dentistApp');
+    await mongoose.connect('mongodb+srv://architshrivas58_db_user:WtVueO8D8TX39kth@cluster0.bcv8vlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error.message);
